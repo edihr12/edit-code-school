cbf58c9c0c1eacc818f3c8a96a8476912e361635
Edi Celar <edi.itk@gmail.com>
2024-05-06 15:17:07 +0200

    Navbar je promijenjen

--- novi-projekt/src/components/Navbar.jsx
+++ novi-projekt/src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useRole } from '../UserRoleContext'; // Ensure this import is correct
 
 function Navbar() {
+    const { role, toggleRole } = useRole(); // Using useContext to access role and toggleRole
+
     return (
         <nav>
             <ul>
                 <li><Link to="/">Workshops</Link></li>
                 <li><Link to="/instructors">Instructors</Link></li>
+                <li>
+                    <label>
+                        Admin:
+                        <input
+                            type="checkbox"
+                            checked={role === 'admin'}
+                            onChange={toggleRole}
+                        />
+                    </label>
+                </li>
             </ul>
         </nav>
     );
